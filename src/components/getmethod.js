c88d1d1e7566263653d7d4ffe56853e4839f86d2
abi4679553 <abivengadajalam7708@gmail.com>
2026-07-17 17:52:59 +0530

    delete

--- src/components/getmethod.js
+++ src/components/getmethod.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Trash2 } from "lucide-react"
 
 const Getmethod = () => {
     const [table, setTable] = useState([]);
@@ -21,9 +22,59 @@ const Getmethod = () => {
             });
     }, []);
 
+    const handleDeleteUser = (id) => {
+        fetch(`http://localhost:5000/delete-user/${id}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    alert(data.message);
+                    window.location.reload();
+                } else {
+                    alert(data.message);
+                }
+            })
+            .catch((err) => {
+                console.log("Error:", err);
+            });
+    }
+
     return (
         <div>
-            <h2>Get Method</h2>
+            <div className="flex justify-center mt-10">
+                <table cellPadding="15" className="border">
+                    <thead className=" bg-blue-50 text-blue-600">
+                        <tr>
+                            <th className="border">S.No</th>
+                            <th className="border"> Name</th>
+
+                            <th className="border">Email</th>
+
+                            <th className="border">password</th>
+
+                            <th className="border">Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {table.map((item, index) => (
+                            <tr key={index}>
+                                <td className="border">{index + 1}</td>
+                                <td className="border">{item.Name || item.name}</td>
+
+                                <td className="border">{item.email}</td>
+                                <td className="border">{item.password}</td>
+                                <td className="border">
+                                    <span onClick={() => handleDeleteUser(item._id)}>{<Trash2 size={20} />}</span>
+                                </td>
+
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
